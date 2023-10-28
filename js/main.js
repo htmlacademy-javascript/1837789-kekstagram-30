93d59da779aa77ceb4eb81ba41ea05f0d0b41ecf
Alina <alinavdovichenko.ru@gmail.com>
2023-10-28 03:15:40 +0300

    Добавление модуля Thambnail

--- js/main.js
+++ js/main.js
@@ -1,20 +1,6 @@
 import {getPictures} from './data.js';
+import {renderThambnails} from './thambnail.js';
 
-const result = getPictures();
+const pictures = getPictures();
 
-const ListPhotos = document.querySelector('.pictures');
-const userPhotosTemplate = document.querySelector('#picture')
-  .content.querySelector('.picture');
-
-const ListPhotosFragment = document.createDocumentFragment();
-
-result.forEach((photo) => {
-  const PhotoElement = userPhotosTemplate.cloneNode(true);
-  PhotoElement.querySelector('.picture__img').src = photo.url;
-  PhotoElement.querySelector('.picture__img').alt = photo.description;
-  PhotoElement.querySelector('.picture__likes').textContent = photo.likes;
-  PhotoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  ListPhotosFragment.appendChild(PhotoElement);
-});
-
-ListPhotos.appendChild(ListPhotosFragment);
+renderThambnails(pictures);
