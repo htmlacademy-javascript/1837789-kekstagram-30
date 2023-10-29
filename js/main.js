82100ce95e9b05afca18a5fef0be732cad410540
Alina <alinavdovichenko.ru@gmail.com>
2023-10-30 02:22:42 +0300

    Атрибуты для формы

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ import { getPictures } from './data.js';
 import { renderGallery } from './gallery.js';
 
 renderGallery(getPictures());
+
+const imgUploadOverlay = document.querySelector('.img-upload__overlay');
+
+imgUploadOverlay.classList.remove('hidden');
